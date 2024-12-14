8081829316ccc6b8e0b26aa2c240c472a7180ef6
MaksFULL <169079873+MaksFULL@users.noreply.github.com>
2024-12-14 15:19:23 +0200

    fdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-
-
-
+import Header from "./Components/Header/header";
 
 
 const App = () => {
   return (
    <>
-   <Header/>
+   <Header />
    <Header author='Oleg' message='Hello!' isOnline={true}/>
    <Header author='Sasha' message='How are you?' isOnline={false}/>
    <Header author='Ann' message='when?' isOnline={true}/>
